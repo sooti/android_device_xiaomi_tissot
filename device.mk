5158c6522f67d315a3a21017b7b2a657b1aa76f9
xiaobai <fengz181x@126.com>
2018-01-06 10:54:33 +0800

    tissot-camera: shim miui camera hal
    Change-Id: Icb571fac9061603fa5d35848eb47c4709d236fd0

--- device.mk
+++ device.mk
@@ -123,18 +123,17 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    MiuiCamera \
-    libshims_skia \
-    Snap
+    Snap \
+    MiuiCamera
 
 PRODUCT_PACKAGES += \
-    android.frameworks.displayservice@1.0_32 \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
-    camera.device@3.3-impl \
-    android.hardware.camera.provider@2.4-impl \
-    vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
+    libshim_MiCamera
 
 # Configstore
 PRODUCT_PACKAGES += \
